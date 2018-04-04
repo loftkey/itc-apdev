76d1ff5a51f3adb24ffd74f3031c4cdf8d01f593
Joshua Marsden <joshuamarsden@gmail.com>
2018-04-04 15:59:28 -0700

    Started creating a javascript function to create modal dialog boxes.

--- js/app.js
+++ js/app.js
@@ -9,3 +9,25 @@
 functon createUI() {
     $("")
 }
+
+function createModal() {
+    <div class="modal fade" id="dialog" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Welcome to the Resume Builder</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    Let's get started!
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <a href="resumebuilder.html"><button type="button" class="btn btn-primary">Continue</button></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+}
--- js/resumebuilder.js
+++ js/resumebuilder.js
@@ -1,1 +1,5 @@
 $("body").append("<aside class=\"sidebar\">This is an aside.</aside>");
+
+$('.word-export').click(function(events){
+                $('.word-content').wordExport();
+});
