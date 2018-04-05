81b9128a94c9e77bf8002df50082b918411afa1b
Joshua Marsden <joshuamarsden@gmail.com>
2018-04-04 18:29:25 -0700

    Revert "Revert "Needed to update index.html to home.html in the main.js script""
    This reverts commit 27cca291c9054873e74228f5b88788559f261bf0.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 http.createServer(function (request, response) {
 
-   fs.readFile("../index.html", function(err, data){
+   fs.readFile("../home.html", function(err, data){
       response.writeHead(200, {'Content-Type': 'text/html'});
       response.write(data);
       response.end();
