449b990d123f5b05d3d2e6c2740728382490dd72
Andrew Price <005475141@coyote.csusb.edu>
2018-04-08 00:03:13 -0700

    added templates and linked to output need to add modal input forms and link data to output html

--- js/home.js
+++ js/home.js
@@ -30,6 +30,23 @@ var lastName = "First Name";
 var eMail = "email@domain.com";
 var summary = "Add an executive summary to highlight your skills, background, and character. Not too long, but enough for your future employer to get to know you.";
 var phoneNum = "123.456.7890";
+var templateNumber = 0;
+function template1()
+{
+    templateNumber = 1;
+}
+function template2()
+{
+    templateNumber = 2;
+}
+function template3()
+{
+    templateNumber = 3;
+}
+function template4()
+{
+    templateNumber = 4;
+}
 function myFunction1()
 {
     firstName = document.getElementById("first_name").value;
@@ -43,22 +60,34 @@ function myFunction1_2()
 }
 function myFunction2() {
     summary = document.getElementById("sum_mary").value;
-     var x = document.getElementById("myDIV");
+    
+     if(templateNumber === 1){
+         var t1 = document.getElementById("template1Element");
+     } else if (templateNumber === 2){
+         var t1 = document.getElementById("template2Element");       
+     } else if (templateNumber === 3){
+         var t1 = document.getElementById("template3Element");
+     } else if (templateNumber === 4){
+         var t1 = document.getElementById("template4Element");       
+     } else {
+         var t1 = document.getElementById("template1Element");
+     }
+     var x = document.getElementById("introductionElement");
      if (x.style.display === "none") {
          x.style.display = "block";
      } else {
          x.style.display = "none";
      }
-     var y = document.getElementById("myDoc");
-    if (y.style.display === "none") {
-         y.style.display = "block";
+//     var t1 = document.getElementById("template1Element");
+    if (t1.style.display === "none") {
+         t1.style.display = "block";
      } else {
-         y.style.display = "none";
+         t1.style.display = "none";
      }
-     document.getElementById("output1").innerHTML = firstName+" "+lastName;
-     document.getElementById("output2").innerHTML = eMail;
-     document.getElementById("output3").innerHTML = summary;
-     document.getElementById("output4").innerHTML = phoneNum;
+     document.getElementById("outputNAME").innerHTML = firstName+" "+lastName;
+     document.getElementById("outputEMAIL").innerHTML = eMail;
+     document.getElementById("outputSUMMARY").innerHTML = summary;
+     document.getElementById("outputPHONE").innerHTML = phoneNum;
 }
 $('.word-export').click(function(events){
                 $('.word-content').wordExport();
